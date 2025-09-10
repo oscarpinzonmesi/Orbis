4c7e5cec1f76251c74814b45532432f04d8b0118
oscarpinzonmesi <oscarpinzon2007@gmail.com>
2025-09-10 14:19:57 -0500

    cambio

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import threading
+import os
 from telegram_bot import iniciar_bot
 
 app = Flask(__name__)
@@ -12,9 +13,10 @@ def run_bot():
     iniciar_bot()
 
 if __name__ == "__main__":
-    # Lanzar bot en segundo plano
+    # Lanzar el bot en segundo plano
     hilo = threading.Thread(target=run_bot, daemon=True)
     hilo.start()
 
-    # Iniciar Flask en el puerto que Render da
-    app.run(host="0.0.0.0", port=10000)
+    # Render exige usar el puerto de la variable de entorno
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
